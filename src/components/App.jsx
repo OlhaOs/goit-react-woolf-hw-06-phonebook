057eebae2ea0c0030e59414ad6a5166c2b9f0c79
OlhaOs <olalaadka@gmail.com>
2024-05-06 10:34:21 +0300

    fix warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
